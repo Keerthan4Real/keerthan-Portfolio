268c268c2c6a8a4d5fc0dbb8aed74624d167eea9
Keerthan <keerthangowda7647@gmail.com>
2025-07-19 13:12:46 +0530

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/portfolio/", // 👈 your repo name
   plugins: [react(), tailwindcss()],
 });
