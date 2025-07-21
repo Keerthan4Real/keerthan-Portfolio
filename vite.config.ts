1ba7042c4e583dc20d1313762c34c0544c47599e
Keerthan <keerthangowda7647@gmail.com>
2025-07-21 19:53:56 +0530

    sec commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/keerthan-Portfolio", // optional
+  base: "/keerthan-Portfolio/", // optional
 });
